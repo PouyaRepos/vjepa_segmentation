efb6962da6e7b9e08dfa6a9e71aad23f37840512
Pouya <pouya.abasi@gmail.com>
2025-08-28 09:52:16 +0200

    Flatten project; remove nested vjepa_segmentation copy

--- vjepa_segmentation/scripts/download_cihp.sh
+++
@@ -1,8 +0,0 @@
-
-# ===========================
-# scripts/download_cihp.sh
-# ===========================
-#!/usr/bin/env bash
-set -e
-mkdir -p data/cihp && cd data/cihp
-echo "Please download CIHP dataset manually (license). Place images under Images/{train,val} and masks under Annotations/{train,val}."
--- vjepa_segmentation/vjepa_segmentation/scripts/demo_video.sh
+++
@@ -1,6 +0,0 @@
-
-# ===========================
-# scripts/demo_video.sh
-# ===========================
-#!/usr/bin/env bash
-python -m vjepa_seg.inference --config configs/cihp_linear_fpn.yaml --video samples/clip.mp4 --out out.mp4 --ema 0.6 --dummy_backbone 1
